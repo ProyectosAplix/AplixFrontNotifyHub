5fddd49b7f567deaa08aa8990b0006bd9b481480
Luis Rivel <luis.rivel@aplix.cr>
2025-05-08 13:32:54 -0600

    Cambios de Roles

--- wb-aplix/src/env-config.js
+++
@@ -1,8 +0,0 @@
-// env-config.js
-const env = {
-    API_URL: 'https://wbusersp.azurewebsites.net/api/',
-    // Define otras variables de entorno aquí según sea necesario
-  };
-  
-  export default env;
-  
